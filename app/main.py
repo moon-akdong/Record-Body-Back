a3eaaa6f8f4ac4047278043adac7e1a4b617a2a5
Moonjeayun <problemy0913@gmail.com>
2026-04-17 02:49:04 +0900

    feat: meal router 연결

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
 from fastapi.middleware.cors import CORSMiddleware
-
+from app.api.user import router as user_router
+from app.api.meal import router as meal_router
 from app.core.config import settings
-
 
 app = FastAPI()
 
@@ -15,6 +15,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(user_router)
+app.include_router(meal_router)
 
 @app.get("/health")
 def health_check() -> dict[str, str]:
