62a4379b07e1b50172255db62ffab7275b5d35fe
Moonjeayun <problemy0913@gmail.com>
2026-04-18 23:42:14 +0900

    fix: OAuth2 인증 변경

--- app/api/user.py
+++ app/api/user.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-
+from fastapi.security import OAuth2PasswordRequestForm
 from app.core.database import get_db
 from app.core.security import create_access_token, get_current_user
 from app.core.dependency import verify_password
@@ -25,16 +25,16 @@ def register(user_create:UserCreate,db:Session=Depends(get_db)):
     }
 
 @router.post("/login")
-def login(user_login:UserLogin, db: Session = Depends(get_db)):
-    user = get_user(user_login.email, db)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = get_user(form_data.username, db)
 
-    if user is None or not verify_password(user_login.password, user.password):
+    if user is None or not verify_password(form_data.password, user.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail= "이메일 또는 비밀번호가 올바르지 않습니다.",
         )
     access_token = create_access_token(
-        data={"sub": str(user.id), "email": user.email},
+        data={"sub": str(user.id), "email": user.username},
     )
 
     return {
@@ -46,7 +46,7 @@ def login(user_login:UserLogin, db: Session = Depends(get_db)):
 def read_me(current_user: User = Depends(get_current_user)):
     return UserResponse(
         email=current_user.email,
-        name=current_user.name,
+        name=current_user.username,
         birth_date=current_user.birth_date,
         gender=current_user.gender,
         height=current_user.height,
