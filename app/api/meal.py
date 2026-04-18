d3fbd79857d5735368781fe61293a7be989883ab
Moonjeayun <problemy0913@gmail.com>
2026-04-18 23:43:07 +0900

    fix: response_model add, 변수 오타 수정

--- app/api/meal.py
+++ app/api/meal.py
@@ -3,19 +3,20 @@ from sqlalchemy.orm import Session
 from app.models.user import User
 from app.core.security import get_current_user
 from app.core.database import get_db
-from app.schemas.meal import MealInput
+from app.schemas.meal import MealInput, CreateMealResponse
 from app.services.meal_service import register_meal_record
 router = APIRouter(prefix="/meals", tags=["meal"])
 
-@router.post("/register")
+@router.post("/register",response_model=CreateMealResponse)
 def meal_create(meal_recod:MealInput, 
                        current_user : User = Depends(get_current_user),
                        db:Session = Depends(get_db)):
     meal_id = register_meal_record(user_id = current_user.id,
-                                   meal_input=meal_recod,
+                                   meal_record=meal_recod,
                                    db=db)
     return {
         "user_id": current_user.id,
         "image_url": meal_recod.image_url,
         "meal_id": meal_id
     }
+
