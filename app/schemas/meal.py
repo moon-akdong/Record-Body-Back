ee18b380cbe9e3d9cdc3997f5621e8b279177b40
Moonjeayun <problemy0913@gmail.com>
2026-04-20 19:21:08 +0900

    feat: 조회 schemas 추가 MealResponse

--- app/schemas/meal.py
+++ app/schemas/meal.py
@@ -20,3 +20,23 @@ class CreateMealResponse(BaseModel):
     user_id:int
     image_url:str
     meal_id:int
+
+
+class MealItemResponse(BaseModel):
+    name: str
+    amount_g: float
+    calories: float
+    carb: float
+    protein: float
+    fat: float
+    sugar: float
+
+class MealResponse(BaseModel):
+    user_id:int
+    eaten_at: datetime
+    total_calories: float
+    total_carb: float
+    total_protein: float
+    total_fat: float
+    total_sugar: float
+    items: list[MealItemResponse]
