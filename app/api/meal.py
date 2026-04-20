248d07db2da58e6414b938d8b1003b7207e9096f
Moonjeayun <problemy0913@gmail.com>
2026-04-20 19:21:49 +0900

    feat: mealRecord조회 API 연결

--- app/api/meal.py
+++ app/api/meal.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from datetime import datetime
 from app.models.user import User
 from app.core.security import get_current_user
 from app.core.database import get_db
-from app.schemas.meal import MealInput, CreateMealResponse
+from app.schemas.meal import MealInput, CreateMealResponse, MealResponse
 from app.services.meal_service import register_meal_record
+from app.services.meal_read import read_meal_id,read_meals_by_date
 router = APIRouter(prefix="/meals", tags=["meal"])
 
 @router.post("/register",response_model=CreateMealResponse)
@@ -20,3 +22,23 @@ def meal_create(meal_recod:MealInput,
         "meal_id": meal_id
     }
 
+@router.get("/{meal_id}", response_model=MealResponse)
+def meal_read(meal_id:int,
+              current_user:User=Depends(get_current_user),
+              db:Session=Depends(get_db)):
+    meal = read_meal_id(meal_id,current_user.id,db)
+    
+    if not meal:
+        raise HTTPException(status_code=404,detail="Meal not Found") 
+
+    return meal
+
+@router.get("/by-date", response_model=list[MealResponse])
+def meal_eaten_read(
+    eaten_at: datetime,
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    meals = read_meals_by_date(eaten_at, current_user.id, db)
+
+    return meals
