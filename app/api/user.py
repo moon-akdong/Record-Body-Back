1cd6cb9d9476b978768de9225f1d3b959005a66c
Moonjeayun <problemy0913@gmail.com>
2026-05-15 16:31:54 +0900

    fix: debuging용 print문 제거

--- app/api/user.py
+++ app/api/user.py
@@ -12,7 +12,6 @@ router = APIRouter(prefix="/users", tags=["users"])
 
 @router.post("/register")
 def register(user_create:UserCreate,db:Session=Depends(get_db)):
-    print(user_create)
     existing_user = get_user(user_create.email, db)
     if existing_user:
         raise HTTPException(
