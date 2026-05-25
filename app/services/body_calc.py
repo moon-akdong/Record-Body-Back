2b30bda65d5a5ad1b79a4cb8c52f2275c0311277
Jaeyun_Moon <problemy0913@gmail.com>
2026-05-25 19:05:58 +0900

    feat: ActivityLevel Enum Setting

--- app/services/body_calc.py
+++ app/services/body_calc.py
@@ -1,3 +1,11 @@
+from enum import Enum
+
+class ActivityLevel(float, Enum):
+    SEDENTARY = 1.2        # 비활동적 (운동 거의 안 함)
+    LIGHT = 1.375          # 가벼운 활동 (주 1-3회)
+    MODERATE = 1.55        # 보통 활동 (주 3-5회)
+    ACTIVE = 1.725         # 활발한 활동 (주 6-7회)
+    VERY_ACTIVE = 1.9      # 매우 활발 (운동선수 수준)
 
 def total_daily_energy_expenditure(gender:str,
                                    age:int,
